1efd9bd7801fb4755e1ac254040b4d68cc22f176
Andy Johnston <hello@eido.studio>
2025-08-28 17:02:08 +0100

    Root tidy up

--- src/components/timeline/sections/index.ts
+++
@@ -1,5 +0,0 @@
-// Timeline sections components
-export { TimelineNavigation } from './TimelineNavigation';
-export { TimelineContent } from './TimelineContent';
-export { AvailabilityContent } from './AvailabilityContent';
-export { TimelineControls } from './TimelineControls';
