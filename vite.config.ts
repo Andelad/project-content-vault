5cce655b7d105a8999b984aaf0e84578d084aabd
Andy Johnston <hello@eido.studio>
2025-11-03 19:02:08 +0000

    Updates

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,11 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+  },
   server: {
     host: "::",
     port: 8080,
